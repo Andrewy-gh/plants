764f5296e5a894a965f6462ef9600a3abafc4d35
Andy-git985 <andy.yu617@gmail.com>
2022-05-23 23:38:37 +0000

    added replit and script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+const showcase = document.querySelector('.showcase')
+
+window.addEventListener("scroll", (event) => {
+    if (this.scrollY > showcase.clientHeight - 20) {
+        document.querySelector('#header').classList.add('border-bottom');
+    } else {
+        document.querySelector('#header').classList.remove('border-bottom');
+    }
+});
